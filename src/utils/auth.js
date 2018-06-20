37805f1d9131021aa275bb96c12138c594ef0b0f
po.lu <lupo.f@outlook.com>
2018-06-20 14:31:12 +0800

    修改tokenkey

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,10 @@
 import Cookies from 'js-cookie'
 
-const TokenKey = 'Admin-Token'
+const TokenKey = 'X-USER-AUTHORIZATION'
+
+export function getTokenKey() {
+  return TokenKey
+}
 
 export function getToken() {
   return Cookies.get(TokenKey)
