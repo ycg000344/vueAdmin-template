c7e5cd19c031c2fd131e366f0a770ade5175e457
po.lu <lupo.f@outlook.com>
2018-07-14 18:50:13 +0800

    // TODO 添加路由模块

--- src/api/basicmanager/routers.js
+++ src/api/basicmanager/routers.js
@@ -1,23 +1,20 @@
 import request from '@/utils/request'
-
 /**
- *查询全部一级菜单
- * @param {*} params
+ * 查询全部一级菜单
  */
-export function getParentRouters(params) {
+export function parents() {
   return request({
-    url: '/basic/router/all',
-    method: 'get',
-    params
+    url: '/basic/router/parents',
+    method: 'get'
   })
 }
 /**
  *分页查询二级菜单
  * @param {*} params
  */
-export function getRouters(params) {
+export function children(params) {
   return request({
-    url: '/basic/router/page',
+    url: '/basic/router/children',
     method: 'get',
     params
   })
--- src/views/basicmanager/routers/index.vue
+++ src/views/basicmanager/routers/index.vue
@@ -2,19 +2,66 @@
     <div class="app-container">
         <el-row :gutter="20">
             <!-- 一级菜单 -->
-            <el-col :span="8">
+            <el-col :span="4">
+                <div>
+                  <h3><span>这里是一级菜单表格区域</span></h3>
+                </div>
                 <div>
                     <ul>
-                        <li v-for="pRouter in parentRouters">
-                            <span>{{pRouter.name}}</span>
+                        <li v-for=" item in parentRouters" :key="item.routerId " @click="change(item.routerId)">
+                            <span >{{item.name}}</span>
+                            <i :class="{ 'el-icon-success' : item.routerId == listQuery.routerParentId }"></i>
                         </li>
                     </ul>
                 </div>
             </el-col>
             <!-- 二级菜单 -->
-            <el-col :span="16">
+            <el-col :span="20">
                 <div>
-
+                  <h3><span>这里是二级菜单表格区域</span></h3>
+                </div>
+                <div>
+                  <el-table :data="list" v-loading.body="listLoading" element-loading-text="Loading" border fit highlight-current-row >
+                    <el-table-column align="center" prop="routerId" label='序号'  >
+                      <template slot-scope="scope">
+                        <span>{{scope.row.routerId}}</span>
+                      </template>
+                    </el-table-column>
+                    <el-table-column align="center" prop="path" label='路由请求路径'  >
+                      <template slot-scope="scope">
+                        <span>{{scope.row.path}}</span>
+                      </template>
+                    </el-table-column>
+                    <el-table-column align="center" prop="component" label='组件'  >
+                      <template slot-scope="scope">
+                        <span>{{scope.row.component}}</span>
+                      </template>
+                    </el-table-column>
+                    <el-table-column align="center" prop="title" label='标题'  >
+                      <template slot-scope="scope">
+                        <span>{{scope.row.title}}</span>
+                      </template>
+                    </el-table-column>
+                    <el-table-column align="center" prop="updateTime" label='更新时间'  >
+                      <template slot-scope="scope">
+                        <span>{{scope.row.updateTime}}</span>
+                      </template>
+                    </el-table-column>                    
+                    <el-table-column align="center" prop="createTime" label='创建时间'  >
+                      <template slot-scope="scope">
+                        <span>{{scope.row.createTime}}</span>
+                      </template>
+                    </el-table-column>   
+                    <el-table-column align="center" prop="status" label='可用状态'  >
+                      <template slot-scope="scope">
+                        <span>{{scope.row.status}}</span>
+                      </template>
+                    </el-table-column>
+                  </el-table>
+                <div class="pagination-container">
+                  <el-pagination background @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="listQuery.page" :page-sizes="[10,20,30, 50]" :page-size="listQuery.limit" layout="total, sizes, prev, pager, next, jumper" :total="total">
+                  </el-pagination>
+                </div>
                 </div>
             </el-col>
         </el-row>
@@ -22,7 +69,7 @@
 </template>
 
 <script>
-import { getParentRouters, getRouters } from '@/api/basicmanager/routers'
+import { parents, children } from '@/api/basicmanager/routers'
 export default {
   name: 'routers-component',
   data() {
@@ -36,35 +83,54 @@ export default {
         limit: 10,
         prop: 'create_time', // 排序字段
         order: 'desc', // 排序方式
-        router_parent_id: -1 // 父路由id，一级路由为-1
+        routerParentId: null
       }
     }
   },
-  created() {
-    console.log('准备获取一级菜单')
-    this.getParentRoutersList()
-  },
   methods: {
-    getParentRoutersList() {
-      getParentRouters().then(response => {
-        // this.parentRouters = response.data.data
-        console.log('response', response.data.data)
+    getParents() {
+      parents().then(response => {
+        this.parentRouters = response.data
+        this.listQuery.routerParentId = response.data[0].routerId
+        console.log(this.listQuery.routerParentId)
+        this.getChildren()
       }).catch(err => {
-        console.log('err', err)
+        console.log(err)
       })
     },
-    getRoutersList() {
+    getChildren() {
       this.listLoading = true
-      getRouters(this.listQuery).then(response => {
-        this.list = response.data.row
+      children(this.listQuery).then(response => {
+        this.list = response.data.rows
         this.total = response.data.total
-        this.listLoading = false
+      }).catch(err => {
+        console.log(err)
       })
+      this.listLoading = false
+    },
+    change(routerId) {
+      this.listQuery.routerParentId = routerId
+      this.getChildren()
+    },
+    handleSizeChange(val) {
+      this.listQuery.limit = val
+      this.getChildren()
+    },
+    handleCurrentChange(val) {
+      this.listQuery.page = val
+      this.getChildren()
     }
+  },
+  created() {
+    this.getParents()
   }
 
 }
 </script>
 
 <style>
+
+
+
+
 </style>
