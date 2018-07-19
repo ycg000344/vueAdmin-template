e77a40e4e8a95a96d1249846ab1e7dd9a47b1e5d
po.lu <lupo.f@outlook.com>
2018-07-19 14:53:47 +0800

    添加h5的二维码

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -26,28 +26,15 @@
           <svg-icon icon-class="eye" />
         </span>
       </el-form-item>
-
-      <el-button type="primary" style="width:100%;margin-bottom:30px;" :loading="loading" @click.native.prevent="handleLogin">{{$t('login.logIn')}}</el-button>
-
-      <!-- <div class="tips">
-        <span>{{$t('login.username')}} : admin</span>
-        <span>{{$t('login.password')}} : {{$t('login.any')}}</span>
-      </div>
-      <div class="tips">
-        <span style="margin-right:18px;">{{$t('login.username')}} : editor</span>
-        <span>{{$t('login.password')}} : {{$t('login.any')}}</span>
-      </div>
-
-      <el-button class="thirdparty-button" type="primary" @click="showDialog=true">{{$t('login.thirdparty')}}</el-button> -->
+      <el-button type="primary" style="width:85%;margin-bottom:30px;" :loading="loading" @click.native.prevent="handleLogin">{{$t('login.logIn')}}</el-button>
+      <!-- <el-button type="primary" @click="showDialog=true">h5</el-button> -->
+      <el-popover 
+        placement="right"
+        trigger="hover">
+        <el-button slot="reference" type="primary">h5</el-button>
+        <img src="@/assets/h5.png" />
+      </el-popover>
     </el-form>
-
-    <!-- <el-dialog :title="$t('login.thirdparty')" :visible.sync="showDialog" append-to-body>
-      {{$t('login.thirdpartyTips')}}
-      <br/>
-      <br/>
-      <br/>
-      <social-sign />
-    </el-dialog> -->
 
   </div>
 </template>
